3443beae4ac582ed572c71180d1a2458e10a3b71
kazuakihiga <higak926@gmail.com>
2021-05-26 09:33:21 +0900

    github oauth2

--- DSprj002/src/main/java/com/DSprj/DSprj002/kajitool/config/WebSecurityConfig.java
+++ DSprj002/src/main/java/com/DSprj/DSprj002/kajitool/config/WebSecurityConfig.java
@@ -1,6 +1,7 @@
 package com.DSprj.DSprj002.kajitool.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -11,7 +12,14 @@ import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
   @Override
   protected void configure(HttpSecurity http) throws Exception {
-	http.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+	http.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+	.and().oauth2Login()
+	.and().authorizeRequests()
+	  .mvcMatchers(HttpMethod.POST, "/**/*").authenticated()
+	  .mvcMatchers(HttpMethod.PUT, "/**/*").authenticated()
+	  .mvcMatchers(HttpMethod.DELETE, "/**/*").authenticated()
+	  .anyRequest().permitAll()
+	;
   }
 
 }
