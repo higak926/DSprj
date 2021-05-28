e32f8372bfca66a9f3ccf28a9a4f8a03da4e15bb
kazuakihiga <higak926@gmail.com>
2021-05-28 15:02:02 +0900

    move angular project

---
+++ DSprj001Ang/src/app/customer/customer.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { Subject } from 'rxjs';
+import { CustomerService } from '../service/customer.service';
+import { takeUntil } from 'rxjs/operators'
+
+@Component({
+  selector: 'app-customer',
+  templateUrl: './customer.component.html',
+  styleUrls: ['./customer.component.scss']
+})
+export class CustomerComponent implements OnInit {
+
+  public serviceProp: String = '';
+  customers: any = [];
+
+  destroy$: Subject<boolean> = new Subject<boolean>();
+
+  constructor(private customerService: CustomerService) { }
+
+  getAllCustomers() {
+    this.customerService.getCustomers().subscribe((customers: any) => {
+      console.log(customers);
+      this.customers = customers;
+      console.log(this.customers);
+    })
+  }
+
+  ngOnInit(): void {
+    this.serviceProp = this.customerService.customerProp;
+    this.getAllCustomers();
+  }
+
+}
