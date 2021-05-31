1de73c155c97f97e208163a12f27fb4acb7a58ce
kazuakihiga <higak926@gmail.com>
2021-05-31 11:17:08 +0900

    persistence state

--- DSprj001Ang/src/app/shop/shop.component.ts
+++ DSprj001Ang/src/app/shop/shop.component.ts
@@ -1,10 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { getEntityType } from '@datorama/akita';
+import { AkitaNgFormsManager } from '@datorama/akita-ng-forms-manager';
 import { Observable } from 'rxjs';
 import { ShopQuery } from './shop.query';
 import { ShopService } from './shop.service';
 import { ShopState } from './shop.store';
+
+export interface FormsState {
+  productForm: {
+    productName: string;
+    price: number;
+  };
+}
 
 @Component({
   selector: 'app-shop',
@@ -16,7 +24,7 @@ export class ShopComponent implements OnInit {
   readonly allProduct$: Observable<getEntityType<ShopState>[]>
   formGroup: FormGroup;
 
-  constructor(private shopService: ShopService, private shopQuery: ShopQuery, private formBuilder: FormBuilder) {
+  constructor(private shopService: ShopService, private shopQuery: ShopQuery, private formBuilder: FormBuilder, private formsManager: AkitaNgFormsManager<FormsState>) {
     this.allProduct$ = this.shopQuery.selectAll();
     this.formGroup = this.formBuilder.group({
       productName: '',
@@ -25,10 +33,15 @@ export class ShopComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.formsManager.upsert('productForm', this.formGroup);
   }
 
   addProduct() {    
     this.shopService.addProduct(this.formGroup.get('productName')!.value, this.formGroup.get('price')!.value);
+  }
+
+  ngOnDestroy() {
+    this.formsManager.unsubscribe();
   }
 
 }
